dbb6a352f79675f0c7c3ed60272089eacf8ca35e
Michelle <michellequintanilla38@gmail.com>
2026-02-11 01:27:01 -0600

    cambio de archivos .env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 //  FUNCIONES PARA CONSUMIR EL BACKEND
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 //  Obtener todos los clientes
 export async function getClients() {
